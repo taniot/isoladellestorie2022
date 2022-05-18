d4991108a38c04b62bacd532bcb47a0b48ec2564
taniot <gaetano.frascolla@gmail.com>
2022-05-18 10:58:30 +0200

    start from clean

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from "next";
-import Image from "next/image";
 import Intro from "../components/intro/intro";
-import styles from "../styles/home.module.scss";
 const Home: NextPage = () => {
   return (
     <>
--- pages/ospiti__.tsx
+++
@@ -1,3 +0,0 @@
-const PageOspiti = () => <div>Pagina Ospiti</div>;
-
-export default PageOspiti;
