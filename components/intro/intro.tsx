4863f25cda50b8eff9b7defe6f571d24c65798f4
taniot <gaetano.frascolla@gmail.com>
2022-05-18 12:01:37 +0200

    style: css mobile

--- components/intro/intro.tsx
+++ components/intro/intro.tsx
@@ -1,5 +1,23 @@
 import styles from "./intro.module.scss";
+import Div100vh from "react-div-100vh";
 
-const Intro = () => <div className={styles.banner}></div>;
+const Intro = () => (
+  <>
+    <Div100vh className={styles.banner}>
+      <div className={styles.slogan}>
+        <h1 className={styles.title}>
+          L’Isola delle
+          <br />
+          Storie
+          <span className={styles.whenTitle}>
+            <span className={styles.place}>Gavoi</span>
+            <span className={styles.date}>01.07 - 03.07.2022</span>
+          </span>
+        </h1>
+        <h2 className={styles.subTitle}>Festival Letterario della Sardegna</h2>
+      </div>
+    </Div100vh>
+  </>
+);
 
 export default Intro;
