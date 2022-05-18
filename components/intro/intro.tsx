92c572e4d0628eccef54f990ebbc21ec92605a3d
taniot <gaetano.frascolla@gmail.com>
2022-05-18 11:48:55 +0200

    style: css mobile

--- components/intro/intro.tsx
+++ components/intro/intro.tsx
@@ -1,18 +1,5 @@
 import styles from "./intro.module.scss";
 
-const Intro = () => (
-  <>
-    <div className={styles.banner}>
-      <div className={styles.slogan}>
-        <h1 className={styles.title}>
-          L’Isola delle
-          <br />
-          Storie
-        </h1>
-        <h2 className={styles.subTitle}>Festival Letterario della Sardegna</h2>
-      </div>
-    </div>
-  </>
-);
+const Intro = () => <div className={styles.banner}></div>;
 
 export default Intro;
