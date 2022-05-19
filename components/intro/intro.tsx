533571e87afe38302a900f8c27af5d6c01a5e8f1
taniot <gaetano.frascolla@gmail.com>
2022-05-20 00:02:29 +0200

    feat test with nothing

--- components/intro/intro.tsx
+++ components/intro/intro.tsx
@@ -4,12 +4,15 @@ import Div100vh from "react-div-100vh";
 const Intro = () => (
   <div className={styles.banner}>
     <div className={styles.slogan}>
-      <h1 className={styles.title}>L’Isola delle Storie</h1>
-      <h2 className={styles.subTitle}>Festival Letterario della Sardegna</h2>
-      <div className={styles.whenTitle}>
-        <span className={styles.place}>Gavoi</span>
-        <span className={styles.date}>01.07 - 03.07.2022</span>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>L’Isola delle Storie</h1>
+        <div className={styles.whenTitle}>
+          <span className={styles.place}>Gavoi</span>
+          <span className={styles.date}>01.07 - 03.07.2022</span>
+        </div>
       </div>
+
+      <h2 className={styles.subTitle}>Festival Letterario della Sardegna</h2>
     </div>
   </div>
 );
