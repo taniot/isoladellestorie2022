e1db2dc7b62c4a827bb2debdd7003d17d025896f
taniot <gaetano.frascolla@gmail.com>
2022-05-20 01:34:58 +0200

    feat test with nothing

--- components/anteprima/anteprima.tsx
+++ components/anteprima/anteprima.tsx
@@ -1,12 +1,17 @@
 import styles from "./anteprima.module.scss";
 
 const Anteprima = () => (
-  <div>
-    <h2>First Names</h2>
-    <p>
-      Mariangela Gualtieri, Daria Bignardi, Paola Saluzzi, Loredana Lipperini,
-      Gabriele Romagnoli, Jonathan Bazzi e molti altri...
-    </p>
+  <div className={styles.container}>
+    <h2>In anteprima</h2>
+    <ul className={styles.hero_list}>
+      <li className={styles.hero_name}>Mariangela Gualtieri</li>
+      <li className={styles.hero_name}>Daria Bignardi</li>
+      <li className={styles.hero_name}>Paola Saluzzi</li>
+      <li className={styles.hero_name}>Loredana Lipperini</li>
+      <li className={styles.hero_name}>Gabriele Romagnoli</li>
+      <li className={styles.hero_name}>Jonathan Bazzi</li>
+      <li className={styles.hero_small}>e molti altri...</li>
+    </ul>
   </div>
 );
 
