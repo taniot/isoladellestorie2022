54c39e55622ba1245b5bcf8b7177895822b77b8c
taniot <gaetano.frascolla@gmail.com>
2022-05-20 10:46:56 +0200

    feat test with nothing

--- components/intro/intro.tsx
+++ components/intro/intro.tsx
@@ -1,27 +1,14 @@
 import styles from "./intro.module.scss";
 import arrow from "./arrow.module.scss";
 import Div100vh from "react-div-100vh";
+Image;
 
 import cls from "classnames";
+import Image from "next/image";
 
 const Intro = () => (
   <div className={styles.banner}>
-    <div className={styles.imgBg}></div>
-    <div className={styles.slogan}>
-      <div className={styles.titleContainer}>
-        <h1 className={styles.title}>L’Isola delle Storie</h1>
-        <div className={styles.whenTitle}>
-          <span className={styles.place}>Gavoi</span>
-          <span className={styles.date}>01.07 - 03.07.2022</span>
-        </div>
-      </div>
-      <h2 className={styles.subTitle}>Festival Letterario della Sardegna</h2>
-      <svg className={arrow.arrows}>
-        <path className={arrow.a1} d="M0 0 L30 32 L60 0"></path>
-        <path className={arrow.a2} d="M0 20 L30 52 L60 20"></path>
-        <path className={arrow.a3} d="M0 40 L30 72 L60 40"></path>
-      </svg>
-    </div>
+    <Image src="/images/test_svg_mobile7.svg" layout="fill" />
   </div>
 );
 
