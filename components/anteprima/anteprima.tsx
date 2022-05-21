7546ee30b696adbb713d7cafd8e79a239e7333c0
taniot <gaetano.frascolla@gmail.com>
2022-05-21 12:30:21 +0200

    feat lets go

--- components/anteprima/anteprima.tsx
+++ components/anteprima/anteprima.tsx
@@ -5,13 +5,26 @@ const Anteprima = () => (
     <div className={styles.container}>
       <h2 className={styles.title}>In questa edizione</h2>
       <ul className={styles.hero_list}>
-        <li className={styles.hero_name}>Mariangela Gualtieri, </li>
-        <li className={styles.hero_name}>Daria Bignardi, </li>
-        <li className={styles.hero_name}>Paola Saluzzi, </li>
-        <li className={styles.hero_name}>Loredana Lipperini, </li>
-        <li className={styles.hero_name}>Gabriele Romagnoli, </li>
+        <li className={styles.hero_name}>
+          Mariangela Gualtieri <span className={styles.trattino}>-</span>{" "}
+        </li>
+        <li className={styles.hero_name}>
+          Daria Bignardi <span className={styles.trattino}>-</span>{" "}
+        </li>
+        <li className={styles.hero_name}>
+          Paola Saluzzi <span className={styles.trattino}>-</span>{" "}
+        </li>
+        <li className={styles.hero_name}>
+          Loredana Lipperini <span className={styles.trattino}>-</span>{" "}
+        </li>
+        <li className={styles.hero_name}>
+          Gabriele Romagnoli <span className={styles.trattino}>-</span>{" "}
+        </li>
         <li className={styles.hero_name}>Jonathan Bazzi </li>
       </ul>
+      <a href="#" className={styles.button}>
+        e molti altri presto online
+      </a>
     </div>
   </section>
 );
