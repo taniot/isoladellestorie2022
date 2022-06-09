86c8ee6ea76fdbafcd88e42e7e541136fd5cee0c
taniot <gaetano.frascolla@gmail.com>
2022-06-09 14:02:10 +0200

    styles cls and lcp

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,6 +1,5 @@
 import styles from "./header.module.scss";
 import Image from "next/image";
-
 import { Squash as Hamburger } from "hamburger-react";
 import Nav from "../nav/nav";
 import { useContext } from "react";
@@ -12,6 +11,7 @@ const Header = () => {
   const context = useContext(AppContext);
   const { state, setIsMainMenuOpen } = context;
   const isSmall = useMediaQuery("(max-width: 1023px)");
+  const isDesktop = useMediaQuery("(min-width: 1024px)");
 
   const closeMenu = (e: { preventDefault: () => void }) => {
     if (setIsMainMenuOpen) setIsMainMenuOpen(false);
@@ -23,19 +23,22 @@ const Header = () => {
         <div className={styles.logoContainer}>
           <Link href="/">
             <a onClick={closeMenu}>
-              {isSmall ? (
+              {isSmall && (
                 <Image
                   src="/images/l-isola-delle-storie-logo-xvii-oriz.svg"
-                  alt="logo"
-                  layout="fill"
+                  alt="L”Isola delle Storie XVII"
+                  width="160px"
+                  height="60px"
                   className={cls(styles.logo, "logoImg")}
                   priority
                 />
-              ) : (
+              )}
+              {isDesktop && (
                 <Image
                   src="/images/l-isola-delle-storie-logo-xvii.svg"
-                  alt="logo"
-                  layout="fill"
+                  alt="L”Isola delle Storie XVII"
+                  width="80px"
+                  height="180px"
                   className={cls(styles.logo, "logoImg")}
                   priority
                 />
@@ -45,13 +48,24 @@ const Header = () => {
         </div>
       </div>
       <div className={cls(styles.menuContainer)}>
-        <Hamburger
-          toggled={state?.isMainMenuOpen}
-          toggle={setIsMainMenuOpen}
-          size={isSmall ? 24 : 30}
-          color={state?.isMainMenuOpen ? "whitesmoke" : "black"}
-          label="Show menu"
-        />
+        {isSmall && (
+          <Hamburger
+            toggled={state?.isMainMenuOpen}
+            toggle={setIsMainMenuOpen}
+            size={24}
+            color={state?.isMainMenuOpen ? "whitesmoke" : "black"}
+            label="Show menu"
+          />
+        )}
+        {isDesktop && (
+          <Hamburger
+            toggled={state?.isMainMenuOpen}
+            toggle={setIsMainMenuOpen}
+            size={30}
+            color={state?.isMainMenuOpen ? "whitesmoke" : "black"}
+            label="Show menu"
+          />
+        )}
       </div>
 
       <Nav />
--- components/intro/intro.tsx
+++ components/intro/intro.tsx
@@ -13,16 +13,32 @@ const Intro = () => {
       <h1 className="hidden">L’Isola delle Storie</h1>
       <h2 className="hidden">Festival Letterario della Sardegna</h2>
       <h3 className="hidden">01.07 - 03.07.2022</h3>
-      {isMobile && (
-        <div className={styles.banner}>
-          <div className={styles.device}>mobile</div>
-          <div className={styles.headerSpace}></div>
-          <div className={styles.sloganSpace}>
-            <div className={styles.slogan}>
+      <div className={styles.banner}>
+        {isMobile && (
+          <>
+            <div className={styles.device}>mobile</div>
+            <div className={styles.headerSpace}></div>
+            <div className={styles.sloganSpace}>
+              <div className={styles.slogan}>
+                <Image
+                  layout="fill"
+                  src="/images/intro-mobile-top.svg"
+                  alt="Isola delle Storie 2022"
+                  priority
+                  placeholder="blur"
+                  blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                    shimmer(800, 600)
+                  )}`}
+                />
+              </div>
+            </div>
+            <div className={styles.imageSpace}>
               <Image
-                layout="fill"
-                src="/images/intro-mobile-top.svg"
+                src="/images/intro-mobile-bottom.png"
                 alt="Isola delle Storie 2022"
+                layout="fill"
+                objectFit="contain"
+                objectPosition="center bottom"
                 priority
                 placeholder="blur"
                 blurDataURL={`data:image/svg+xml;base64,${toBase64(
@@ -30,12 +46,16 @@ const Intro = () => {
                 )}`}
               />
             </div>
-          </div>
-          <div className={styles.imageSpace}>
+          </>
+        )}
+
+        {isTablet && (
+          <>
+            <div className={styles.device}>tablet</div>
             <Image
-              src="/images/intro-mobile-bottom.png"
-              alt="Isola delle Storie 2022"
+              src="/images/intro-desktop.png"
               layout="fill"
+              alt="Isola delle Storie 2022"
               objectFit="contain"
               objectPosition="center bottom"
               priority
@@ -44,43 +64,25 @@ const Intro = () => {
                 shimmer(800, 600)
               )}`}
             />
-          </div>
-        </div>
-      )}
-
-      {isTablet && (
-        <div className={styles.banner}>
-          <div className={styles.device}>tablet</div>
-          <Image
-            src="/images/intro-desktop.png"
-            layout="fill"
-            alt="Isola delle Storie 2022"
-            objectFit="contain"
-            objectPosition="center bottom"
-            priority
-            placeholder="blur"
-            blurDataURL={`data:image/svg+xml;base64,${toBase64(
-              shimmer(800, 600)
-            )}`}
-          />
-        </div>
-      )}
-      {isDesktop && (
-        <div className={styles.banner}>
-          <div className={styles.device}>desktop</div>
-          <Image
-            src="/images/intro-desktop.png"
-            layout="fill"
-            alt="Isola delle Storie 2022"
-            objectFit="contain"
-            objectPosition="center bottom"
-            placeholder="blur"
-            blurDataURL={`data:image/svg+xml;base64,${toBase64(
-              shimmer(800, 600)
-            )}`}
-          />
-        </div>
-      )}
+          </>
+        )}
+        {isDesktop && (
+          <>
+            <div className={styles.device}>desktop</div>
+            <Image
+              src="/images/intro-desktop.png"
+              layout="fill"
+              alt="Isola delle Storie 2022"
+              objectFit="contain"
+              objectPosition="center bottom"
+              placeholder="blur"
+              blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                shimmer(800, 600)
+              )}`}
+            />
+          </>
+        )}
+      </div>
     </>
   );
 };
